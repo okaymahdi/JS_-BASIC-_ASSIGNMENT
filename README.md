2c37148390b62fbf9e4dc918515cc74dfe387519
Md. Mahdiul Hasan <devmahdimunshee@gmail.com>
2024-12-02 22:31:43 +0600

    Second Problem Solved

---
+++ README.md
@@ -0,0 +1,3 @@
+Link_1: https://earthy-cycle-1c2.notion.site/JavaScript-Basic-Problem-Solving-888efe6383904327b3ff6e1b706835b3
+
+Link_2: https://amber-pincushion-314.notion.site/JavaScript-Basic-Problem-Solving-302f28f08bec4080b645cdb954625cc8
--- app.js
+++ app.js
@@ -2,3 +2,10 @@
 // Create a variable called carName, assign the value Volvo to it.
 let carName = 'Volvo'
 console.log(carName)
+
+// 2. On one single line, declare three variables with the following names and values:
+// On one single line, declare three variables with the following names and values:
+let firstName = 'John',
+  lastName = 'Doe',
+  age = 30
+console.log(firstName, lastName, age)
